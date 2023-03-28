ee1f405a7e827364afab0127f8508a659b911a71
Gustavo032 <gustavoramosrock@gmail.com>
2023-03-28 11:45:57 -0300

    tipos de http: GET, Post etc.

--- src/server.js
+++ src/server.js
@@ -1,17 +1,36 @@
-// Aplicações HTTP = api.
 import http from "node:http"
-// commonJS = require
-// ESmodules = import/export 
 
-// import http from "node:http" // modulo interno
-// import fastify from "fastify" // modulo terceiro 
+// - Criar um usuário
+// - Remover um usuário
+// - Listagem de usuários 
+// - Edição de usuários
+
+// - HTTP
+//	 	- Método HTTP
+// 		- URL 
 
 
-// criar um usuário
+// GET, POST, PUT, PATCH, DELETE
+
+// GET => Buscar uma informação no backend
+// Post => Criar uma informação no backend
+// Put => Editar um recurso no backend (atualizar varios ao mesmo tempo(geral))
+// Patch => Atualizar um recurso no backend (atualizar uma informação especifica(tipo um toggle de aceito os termos)
+// DELETE => Deletar um recurso no backend
+
 const server = http.createServer((req, res)=>{
 
 	const { method, url } = req
 	console.log(method, url)
+	
+	if(method === "GET" && url === "/users"){
+		return res.end("Listagem de usuários")
+	}
+	
+	if(method === "POST" && url === "/users"){
+		return res.end("Criação de usuário")
+	}
+
 	return res.end("hello ignite")
 })
 
