3ed8ab7239a5271d0ce2a7f5abd9b357b03e6857
Gustavo032 <gustavoramosrock@gmail.com>
2023-05-03 08:15:43 -0300

    consumindo a stream somente quando completar tudo

--- streams/fake-upload-to-http-stream.js
+++ streams/fake-upload-to-http-stream.js
@@ -7,7 +7,7 @@ class OneToHundredString extends Readable {
 		const i = this.index++
 		
 		setTimeout(()=>{
-			if( i >= 1000){
+			if( i >= 100){
 				this.push(null)
 			}else{
 				const buf = Buffer.from(String(i))
@@ -22,4 +22,8 @@ fetch('http://localhost:3334/', {
 	method: 'POST',
 	body: new OneToHundredString(),
 	duplex: 'half'
+}).then(res=>{
+	return res.text()
+}).then((data)=>{
+	console.log(data);
 })
--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -16,10 +16,23 @@ class InverseNumberStream extends Transform{
 // req => Readable (ler dados)
 // req => Writable (escrever dados)
 
-const server = http.createServer((req,res)=>{
-	return req
-		.pipe(new InverseNumberStream())
-		.pipe(res)
+const server = http.createServer(async(req,res)=>{
+
+	const buffers = []
+
+	for await (const chunk of req){
+		buffers.push(chunk)
+	}
+
+	const fullStreamContent = Buffer.concat(buffers).toString()
+
+	console.log(fullStreamContent)
+
+	return res.end(fullStreamContent)
+
+	// return req
+	// 	.pipe(new InverseNumberStream())
+	// 	.pipe(res)
 	
 })
 
