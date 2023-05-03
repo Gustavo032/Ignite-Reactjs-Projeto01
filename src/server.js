acc1c1578628866846fcd22c98bf690fef389ae7
Gustavo032 <gustavoramosrock@gmail.com>
2023-05-03 14:12:18 -0300

    persistindo banco de dados

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import http from "node:http"
-import { json } from "./middlewares/json"
+import { Database } from "./database.js"
+import { json } from "./middlewares/json.js"
 
 // stateful = Informação em memória para funcionar(tipo temporária)
 // stateless = Informação em banco de dados para funcionar
 
-const users = []
+const database = new Database()
+
+database.database
 
 const server = http.createServer(async(req, res)=>{
 
@@ -12,22 +15,27 @@ const server = http.createServer(async(req, res)=>{
 	
 	console.log(method, url) // imprimindo método e caminho da request
 
-	await json(req,res) // middleware que define body da request
+	await json(req, res) // middleware que define body da request
 	
 	console.log(req.body) // Imprimindo o body da request
 	
 	if(method === "GET" && url === "/users"){ // GET na rota "users"
-		return res
-		.end(JSON.stringify(users))
+		const users = database.select('users')
+
+		return res.end(JSON.stringify(users)) // lista os usuários no DB
 	}
 	
 	if(method === "POST" && url === "/users"){ // POST na rota "users"
 		const { name, email } = req.body
-		users.push({ 
+		
+		const user = { 
 			id: 1, 
-			name: name, 
-			email: email
-		})
+			name, 
+			email,
+		}
+		
+		database.insert("users", user)// registra os usuários no DB
+		
 		return res.writeHead(201).end()
 	}
 
